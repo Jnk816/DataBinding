2511d7fec4ad9c1e1efb7c44b5656db2ea3bd68c
anreh <anurehka@gmail.com>
2022-07-20 11:39:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var student: Student
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //if java -> Student student = new Student()
+
+
+        binding.btnSetValue.setOnClickListener(){
+            student = Student("Ali", 20, "RSD")
+            binding.myData = student
+        }
+
+        //todo:: display student info
+        //example: if use plaintext in design, we can use binding.editTextTextPersonName2.setText(student.name)
+        binding.btnDisplay.setOnClickListener() {
+            student.programme = "RIS"
+
+            binding.apply {
+                invalidateAll() //auto refresh the data
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Student.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+data class Student(var name:String, var age:Int, var programme:String)
+//if want default value can add = behind the data type of each variables
